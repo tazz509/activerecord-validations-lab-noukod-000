9d19becac8bcb96d6acd8bca302c41f7650f9fa0
carly <carly@noukod.com>
2019-01-28 15:29:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+class Clickbait < ActiveModel::Validator
+  def validate(record)
+    messages = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+      if record.title
+        unless messages.any? { |msg| record.title.include? msg }
+          record.errors[:title] << 'the title does not contain the words'
+      end
+      end
+    end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+   validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: {maximum: 250 }
+   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    # message: "%{value} is not a valid" }
+    validates_with Clickbait
+
 end
